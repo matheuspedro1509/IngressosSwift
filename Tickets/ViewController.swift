4118ec04ae1d2fd82a75028eea0a128cf4bfc4b9
Matheus Santana <matheus.pedro@resourceit.com>
2018-02-06 11:07:30 -0200

    Initial Commit

---
+++ Tickets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tickets
+//
+//  Created by Nutela on 06/02/18.
+//  Copyright © 2018 Matheus S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
